5215be7eb1617b9b7c5bde3dd880d2c582362d38
SoundaryaJagan <38034280+SoundaryaJagan@users.noreply.github.com>
2018-07-03 15:54:10 +0000

    Create 123.py

---
+++ 123.py
@@ -0,0 +1,16 @@
+num=int(input())
+sec=int(input())
+li=[]
+a=[]
+for i in range(0,num):
+li.append(int(input()))
+for i in range(0,sec);
+s=0
+u=int(input())
+v=int(input())
+maxi=li[u-1]
+for j in range(u,v):
+if(maxi<li[j]):
+maxi=li[j]
+a.append(maxi)
+print(a)
